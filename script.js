8c31d84539bca6d667797ebfe648ed7b6d00cc22
Gopinath M <146287451+gopinathm01@users.noreply.github.com>
2023-11-20 23:30:22 +0530

    bikes

---
+++ script.js
@@ -0,0 +1,11 @@
+function big(im)
+{
+    document.querySelector('.big').src= im;
+} 
+
+
+function path(color) 
+{
+    const pa = document.querySelector('.path'); 
+    pa.style.background = color;
+}
